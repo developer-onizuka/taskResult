14e141479ce558cd35a551fee674bc28afe4c40f
developer-onizuka <developer@example.com>
2023-06-13 11:37:31 +0000

    2023-06-13

---
+++ ChildAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class ChildAsync
+{
+    public async Task<int> RunTaskAAsync()
+    {
+        var result = await Task.Run(RunTaskB); // Task<int>化されるのでawaitできる
+        //Console.WriteLine(result);
+        return result + 1;
+    }
+
+    public int RunTaskB() // 同期メソッド
+    {
+        return 1 + 2 + 3;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace taskResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildAsync task = new ChildAsync();
+            Console.WriteLine(task.RunTaskAAsync().Result);
+            //int a = x.RunTaskAAsync().Result;
+            //Console.WriteLine(a);
+            Console.WriteLine(task.RunTaskB());
+        }
+    }
+}
